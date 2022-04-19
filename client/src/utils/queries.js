185eb46f467f0a60289e2059242432f16fc37d0d
Ian L <ileach81@gmail.com>
2022-04-18 22:31:08 -0400

    added GETME query

---
+++ client/src/utils/queries.js
@@ -0,0 +1,22 @@
+//imports
+import { gql } from "@apollo/client";
+
+//query me
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
