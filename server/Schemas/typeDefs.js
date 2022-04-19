31d3de2b724251f4a575d11c88cd0fc4c0c3c8db
Ian L <ileach81@gmail.com>
2022-04-18 21:43:27 -0400

    set up type def titles

---
+++ server/Schemas/typeDefs.js
@@ -0,0 +1,19 @@
+//import graphql
+const { gql } = require("apollo-server-express");
+// define typedefs
+const typeDefs = gql`
+  type Query {
+
+  }
+    type Mutation {
+
+  }
+    type User {
+
+  }
+    type Book {
+
+  }
+    type Auth {
+
+  }`;
