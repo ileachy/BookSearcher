c06a8326a8649fb5375678879e0dde2d155bd7b3
Ian L <ileach81@gmail.com>
2022-04-18 21:53:00 -0400

    added typedefs and input type

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -2,18 +2,39 @@
 const { gql } = require("apollo-server-express");
 // define typedefs
 const typeDefs = gql`
+  input SearchBook {
+    bookId: String
+    authers: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
   type Query {
-
+    me: User
   }
-    type Mutation {
-
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(new)
+    removeBook()
   }
-    type User {
-
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
   }
-    type Book {
-
+  type Book {
+    bookId: String
+    authers: [String]
+    title: String
+    description: String
+    image: String
+    link: String
   }
-    type Auth {
-
+  type Auth {
+    token: ID!
+    user: User
   }`;
