62cb7c644a6b4d6fc6b60bf3049b6036adc90f62
Ian L <ileach81@gmail.com>
2022-04-18 22:36:34 -0400

    added login mutation

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,18 @@
+//imports
+import { gql } from "@apollo/client";
+
+// mutations
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+export const ADD_USER = gql``;
+export const SAVE_BOOK = gql``;
+export const REMOVE_BOOK = gql``;
