1fff92291299ed0b7640e4724a1d43efda2bec87
Ian L <ileach81@gmail.com>
2022-04-18 22:09:19 -0400

    forgot to save before last commit

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -4,6 +4,23 @@ const { AuthenticationError } = require("apollo-server-express");
 const { User, Book } = require("../models");
 
 const resolvers = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    // find one user by id
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const uData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return uData;
+      }
+      // validation error handle
+      throw new AuthenticationError("Must be logged in!");
+    },
+  },
+  Mutation: {
+    login: async () => {},
+    addUser: async () => {},
+    saveBook: async () => {},
+    removeBook: async () => {},
+  },
 };
